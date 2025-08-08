7ecbe7c059bf6814b55f5f62b63756d1c03fb60a
Vincent Coraldean <info@vincentcoraldean.com>
2025-08-08 15:09:47 +0100

    Improve conditional regex

--- util/regex.js
+++ util/regex.js
@@ -2,4 +2,4 @@ export const URL_REGEX = /(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+/g;
 
 export const VARIABLE_REGEX = /<<var:([^>]+)>>/g;
 
-export const CONDITIONAL_REGEX = /if .+[:,] /gi;
+export const CONDITIONAL_REGEX = /if.*\<span .*\>.*\<\/span\>.*[:,] /gi;
