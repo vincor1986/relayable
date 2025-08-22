3630799c303bb9713b6458dc7507eb3acc24baf5
Vincent Coraldean <info@vincentcoraldean.com>
2025-08-22 13:57:17 +0100

    Update BASE_URL definition for SSR

--- util/URL.js
+++ util/URL.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 3000;
+const prod = process.env.PRODUCTION === "true" && !process.env.CI;
 
-export const BASE_URL = process.env.PRODUCTION
+export const BASE_URL = prod
   ? "https://www.relayable.dev"
   : `http://localhost:${port}`;
